68c3441749dce5591ad8888a18193ee87a1d7f4d
BRENDACHEBET1 <chebetb388@gmail.com>
2026-05-03 22:54:55 +0300

    Modify components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
       <div className="mx-auto flex items-center justify-between px-6 py-4">
 
         {/* Title */}
-        <h1 style={{ fontFamily: "'Great Vibes', cursive" }} className="text-3xl sm:text-4xl text-blue-900 italic md:text-5xl hover:scale-105 text-center md:text-left ">Portfolio Website</h1>
+        <h1 style={{ fontFamily: "'Great Vibes', cursive" }} className="text-3xl sm:text-4xl text-blue-900 italic md:text-5xl hover:scale-105 text-center md:text-left ">PortfolioWebsite</h1>
         
         {/* Navbar */}
         <NavBar />
